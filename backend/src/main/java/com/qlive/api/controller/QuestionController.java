56ccc8758a069b55c8fc82d9a72b27f87ae7d6de
Doug Barrett <dk.barrett@hotmail.com>
2019-07-20 19:53:13 +1200

    Previous commit missed some changes

--- backend/src/main/java/com/qlive/api/controller/QuestionController.java
+++ backend/src/main/java/com/qlive/api/controller/QuestionController.java
@@ -1,4 +1,15 @@
 package com.qlive.api.controller;
 
+import com.qlive.api.model.Question;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/question")
 public class QuestionController {
+
+    @GetMapping(path="/", produces = "application/json")
+    public Question fetchQuestion()
+    {
+        return new Question("What is the time?");
+    }
 }
