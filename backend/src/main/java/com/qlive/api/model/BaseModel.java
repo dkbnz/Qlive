1cd30b1be7a64bbbaefa51abbf71c5514c4f9f8f
Doug Barrett <mail+git@dkb.nz>
2019-07-20 19:53:13 +1200

    Previous commit missed some changes

--- backend/src/main/java/com/qlive/api/model/BaseModel.java
+++ backend/src/main/java/com/qlive/api/model/BaseModel.java
@@ -1,4 +1,22 @@
 package com.qlive.api.model;
 
-public class BaseModel {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
