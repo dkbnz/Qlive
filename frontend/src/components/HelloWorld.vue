1505e5797503cb2591c64b98ac176ecd17a47fdd
Doug Barrett <mail+git@dkb.nz>
2019-07-20 13:43:58 +1200

    Installed element UI as a UI Framework

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -80,6 +80,7 @@
         </a>
       </li>
     </ul>
+    <el-checkbox v-model="checked">Option</el-checkbox>
   </div>
 </template>
 
@@ -88,6 +89,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      checked: true,
       msg: 'Welcome to Your Vue.js App'
     }
   }
