5570de2629ca8008c9e9cbc89b09fd28cbf44608
Doug Barrett <dk.barrett@hotmail.com>
2019-07-20 17:45:54 +1200

    Basic index page accepting a code to fetch quiz and a button to make a quiz

---
+++ frontend/src/components/Index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="index">
+      <el-row type="flex" justify="center">
+        <h1>Enter code</h1>
+      </el-row>
+      <el-row type="flex" justify="center">
+        <el-col :xs="24" :sm="15" :md="13" :lg="11" :xl="9">
+          <code-input @code-submit="runCommand"></code-input>
+        </el-col>
+      </el-row>
+      <el-row type="flex" justify="center">
+        <h2>or</h2>
+      </el-row>
+      <el-row type="flex" justify="center">
+        <el-button type="primary" icon="el-icon-edit-outline">Create Quiz</el-button>
+      </el-row>
+    </div>
+</template>
+
+<script>
+    import CodeInput from "./CodeInput";
+    export default {
+        name: "Index",
+      methods: {
+          runCommand(code) {
+            console.log(code)
+          }
+      },
+      components: {CodeInput}
+    }
+</script>
+
+<style scoped>
+</style>
