982322063786cfb3ec698ff10b79ae5e1324c1a4
dkbarrett <dk.barrett+gh@hotmail.com>
2019-07-20 19:53:13 +1200

    Previous commit missed some changes

--- backend/src/main/java/com/qlive/api/model/Question.java
+++ backend/src/main/java/com/qlive/api/model/Question.java
@@ -1,4 +1,18 @@
 package com.qlive.api.model;
 
-public class Question {
+public class Question extends BaseModel {
+
+    private String questionText;
+
+    public Question() {}
+
+    public Question(String questionText) { this.questionText = questionText; }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
 }
