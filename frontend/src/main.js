372d219d29d83bb474c9cf22bd78cc8d8b73bf89
dkbarrett <dk.barrett+gh@hotmail.com>
2019-07-21 01:22:09 +1200

    Added axios for api consumption

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,10 @@ import App from './App';
 import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import axios from "axios";
+
+Vue.prototype.$http = axios;
+Vue.prototype.axios = axios;
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
