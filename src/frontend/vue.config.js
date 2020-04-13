2b35787791a0bb9c3f52684d9d3f10fc2fa092ad
Doug Barrett <mail+git@dkb.nz>
2020-04-12 21:45:54 -0500

    Fixed project build directories.

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   outputDir: 'target/dist/',
+
   devServer: {
     proxy: 'http://localhost:9000'
+  },
+
+  assetsDir: 'static',
+
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+          args[0].title = 'Qlive - Live questionnaire app'
+          return args
+        })
   }
 }
