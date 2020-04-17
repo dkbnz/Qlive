123f70f894ec6fbd943853d326eb5268d2f74385
dkbarrett <dk.barrett+gh@hotmail.com>
2020-04-16 23:49:35 -0500

    Basic creation of questions list on frontend. Need to sort some alignment issues.

---
+++ src/frontend/src/components/Index/QuestionSearch.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <el-input placeholder="Search public questions..." v-model="questionSearchQuery">
+            <el-button slot="append" icon="el-icon-arrow-right"></el-button>
+        </el-input>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "QuestionSearch",
+        data: function() {
+            return {
+                questionSearchQuery: ""
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
