9e7daa5572cc80c541aa9e525c1e0f5fffd3730f
dkbarrett <dk.barrett@hotmail.com>
2020-04-16 23:49:35 -0500

    Basic creation of questions list on frontend. Need to sort some alignment issues.

---
+++ src/frontend/src/components/Index/PublicQuestions.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <question-search>
+                </question-search>
+            </div>
+            <question-list>
+            </question-list>
+        </el-card>
+    </div>
+</template>
+
+<script>
+    import QuestionList from "./QuestionList";
+    import QuestionSearch from "./QuestionSearch";
+    export default {
+        name: "PublicQuestions",
+        components: {QuestionSearch, QuestionList}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/frontend/src/components/Index/QuestionList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <el-row @click.native="count+=1"
+                v-bind:key="i"
+                class="grid-content"
+                type="flex"
+                justify="space-around"
+                v-for="i in count">
+            <el-col :span="20">
+                <h5>List group item heading</h5>
+            </el-col>
+            <el-col :span="4" class="p">
+                <small>{{ 10 + i }} Jan '20 | </small>
+                <small><i class="el-icon-s-custom"></i> {{i}} votes</small>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "QuestionList",
+        data () {
+            return {
+                count: 10
+            }
+        },
+        methods: {
+            load () {
+                this.count += 2
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .grid-content {
+        background: #d3dce6;
+        border-radius: 6px;
+        min-height: 36px;
+    }
+
+    .grid-content:hover {
+        background: rgba(178, 201, 226, 0.96);
+    }
+
+    .el-row {
+        margin-bottom: 10px;
+    }
+</style>
