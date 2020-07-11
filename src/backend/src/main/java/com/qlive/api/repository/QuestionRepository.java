02d43e8bc28e438ecfa8236846d7e62f677edffb
Doug Barrett <mail+git@dkb.nz>
2020-07-10 22:53:38 -0500

    Implement backend for querying questions. Need to paginate.

--- src/backend/src/main/java/com/qlive/api/repository/QuestionRepository.java
+++ src/backend/src/main/java/com/qlive/api/repository/QuestionRepository.java
@@ -3,5 +3,9 @@ package com.qlive.api.repository;
 import com.qlive.api.model.Question;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends CrudRepository<Question, String> {
+    List<Question> findQuestionsByIsPublicTrueOrderByCreatedDesc();
+    List<Question> findQuestionsByQuestionTextContainsAndIsPublicTrueOrderByCreatedDesc(String questionText);
 }
