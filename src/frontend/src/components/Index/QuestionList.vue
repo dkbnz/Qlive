d1e5b5e622f1fdef7f39e1140041d4a700ab9719
dkbarrett <dk.barrett@hotmail.com>
2020-07-10 20:04:10 -0500

    Placed search card on index. Refactored index.

--- src/frontend/src/components/Index/QuestionList.vue
+++ src/frontend/src/components/Index/QuestionList.vue
@@ -1,18 +1,20 @@
 <template>
     <div>
-        <el-row @click.native="$emit('question-selected', question.id)"
-                v-bind:key="question.id"
+        <el-row v-bind:key="question.id"
                 class="grid-content"
                 type="flex"
                 justify="space-between"
                 align="middle"
                 v-for="question in questions">
-            <el-col>
-                <h5>{{question.questionText}}</h5>
+            <el-col @click.native="$emit('question-selected', question.id)">
+                <el-link target="_blank">{{ question.questionText }}</el-link>
             </el-col>
-            <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="2">
+            <el-col :xs="11" :sm="8" :md="8" :lg="7" :xl="7">
                 <el-row align="middle">
-                    <small><i class="el-icon-time"></i> {{ parseDate(question.date) }}</small>
+                    <small><i class="el-icon-date"></i> {{ parseDate(question.date) }}</small>
+                </el-row>
+                <el-row align="middle">
+                    <small><i class="el-icon-time"></i> {{ parseTime(question.date) }}</small>
                 </el-row>
                 <el-row align="middle">
                     <small><i class="el-icon-s-custom"></i> {{ question.votes }} vote{{ question.votes !== 1 ? 's' : '' }}</small>
@@ -25,33 +27,21 @@
 <script>
     export default {
         name: "QuestionList",
+        props: {
+            questions: Array
+        },
         data () {
             return {
-                questions: [
-                    {
-                        id: "TS4D5A",
-                        questionText: "This is some question text",
-                        votes: 0,
-                        date: 1591047363
-                    },
-                    {
-                        id: "HGWX5L",
-                        questionText: "This is some question text, maybe even a little longer.",
-                        votes: 1,
-                        date: 1501049082
-                    },
-                    {
-                        id: "DHP3A9",
-                        questionText: "This is some question text, maybe even a little longer. Possibly even a little longer than that",
-                        votes: 15000,
-                        date: 1500000000
-                    }
-                ]
+                selected: ""
             }
         },
         methods: {
             parseDate(timestamp) {
-                timestamp = this.$moment(timestamp*1000).calendar()
+                timestamp = this.$moment(timestamp*1000).format('L')
+                return timestamp
+            },
+            parseTime(timestamp) {
+                timestamp = this.$moment(timestamp*1000).format('LT');
                 return timestamp
             }
         }
@@ -64,9 +54,5 @@
         border-radius: 6px;
         min-height: 36px;
         margin-bottom: 10px;
-    }
-
-    .grid-content:hover {
-        background: rgba(178, 201, 226, 0.96);
     }
 </style>
