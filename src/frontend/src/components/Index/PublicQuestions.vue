ea5cb36137e452d991fe56c45f20cb6248a4ecab
dkbarrett <dk.barrett+gh@hotmail.com>
2020-07-10 20:04:10 -0500

    Placed search card on index. Refactored index.

--- src/frontend/src/components/Index/PublicQuestions.vue
+++ src/frontend/src/components/Index/PublicQuestions.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
-        <el-card class="box-card">
+        <el-card shadow="never">
             <div slot="header" class="clearfix">
-                <question-search @search="fetchResults">
+                <question-search
+                        @search="fetchResults"
+                >
                 </question-search>
             </div>
-            <question-list @question-selected="questionSelected">
+            <question-list
+                    :questions="questions"
+                    @question-selected="questionSelected"
+                    v-loading="loadingQuestions"
+            >
             </question-list>
         </el-card>
     </div>
@@ -17,12 +23,39 @@
     export default {
         name: "PublicQuestions",
         components: {QuestionSearch, QuestionList},
+        data: function() {
+            return {
+                loadingQuestions: false,
+                questions: [
+                    {
+                        id: "TS4D5A",
+                        questionText: "This is some question text",
+                        votes: 0,
+                        date: 1591047363
+                    },
+                    {
+                        id: "HGWX5L",
+                        questionText: "This is some question text, maybe even a little longer.",
+                        votes: 1,
+                        date: 1501049082
+                    },
+                    {
+                        id: "DHP3A9",
+                        questionText: "This is some question text, maybe even a little longer. Possibly even a little longer than that",
+                        votes: 100,
+                        date: 1500000000
+                    }
+                ]
+            }
+        },
         methods: {
             fetchResults(query) {
+                this.loadingQuestions = true
+                // Do the query here
                 console.log(query)
             },
             questionSelected(id) {
-                console.log(id)
+                this.$emit('question-select', id)
             }
         }
     }
