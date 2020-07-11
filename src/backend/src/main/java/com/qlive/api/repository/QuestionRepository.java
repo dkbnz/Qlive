db559482494d514238e2799a2e68d0b08c67e78a
dkbarrett <dk.barrett@hotmail.com>
2020-07-11 17:02:38 +1200

    Added infinite scroll for the listing of questions.

--- src/backend/src/main/java/com/qlive/api/repository/QuestionRepository.java
+++ src/backend/src/main/java/com/qlive/api/repository/QuestionRepository.java
@@ -1,11 +1,12 @@
 package com.qlive.api.repository;
 
 import com.qlive.api.model.Question;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
 
-public interface QuestionRepository extends CrudRepository<Question, String> {
-    List<Question> findQuestionsByIsPublicTrueOrderByCreatedDesc();
-    List<Question> findQuestionsByQuestionTextContainsAndIsPublicTrueOrderByCreatedDesc(String questionText);
+public interface QuestionRepository extends PagingAndSortingRepository<Question, String> {
+    Page<Question> findQuestionsByIsPublicTrueOrderByCreatedDesc(Pageable restrictions);
+    Page<Question> findQuestionsByQuestionTextContainsAndIsPublicTrueOrderByCreatedDesc(String query, Pageable restrictions);
 }
